b6fc98ac683d4488962a8c3b401381375e58392b
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-03-27 20:00:50 -0500

    commiting blah

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,6 @@
 declare class s3Controller {
+    bucket: any;
+    S3: any;
     constructor(config: any);
     addFile: (id: any, model: any, files: any) => Promise<any[]>;
     deleteFile: (files: any) => Promise<void>;
